8d2ab94237d1d5fac1492f8b6e2572e3f5b10b45
Luc Gregoire <lucgmix@gmail.com>
2021-02-16 15:11:11 -0500

    Added minHeight to ErrorMessage so the form doesn't move hen displaying errors.

--- app/components/forms/ErrorMessage.js
+++ app/components/forms/ErrorMessage.js
@@ -1,20 +1,29 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 import Text from "../Text";
 
 function ErrorMessage({ error, visible, style, ...otherProps }) {
-  if (!visible || !error) return null;
-
   return (
-    <Text {...otherProps} style={[styles.error, style]}>
-      {error}
-    </Text>
+    <View style={styles.container}>
+      {visible && (
+        <Text {...otherProps} style={[styles.error, style]}>
+          {error}
+        </Text>
+      )}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  error: { color: "red", fontSize: 12, marginLeft: 16 },
+  container: {
+    minHeight: 16,
+  },
+  error: {
+    color: "red",
+    fontSize: 12,
+    marginLeft: 16,
+  },
 });
 
 export default ErrorMessage;
