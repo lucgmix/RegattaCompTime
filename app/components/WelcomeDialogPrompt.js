c4a1ed6cfaa4b66cb86b7fa692ddcbe96fb2c589
Luc Gregoire <lucgmix@gmail.com>
2021-06-03 15:43:48 -0400

    Implemented UI for Welcome Screen.

--- app/components/WelcomeDialogPrompt.js
+++ app/components/WelcomeDialogPrompt.js
@@ -1,24 +1,24 @@
 import React, { useState } from "react";
-import { Image, View, StyleSheet, Dimensions, ScrollView } from "react-native";
-// import { Overlay } from "react-native-elements";
+import { Image, Dimensions, View, ScrollView, StyleSheet } from "react-native";
+import { Overlay } from "react-native-elements";
 import Text from "./Text";
 import Button from "./Button";
+import { Dialog } from "react-native-paper";
 import defaultStyles from "../config/styles";
-import CheckBox from "react-native-check-box";
+import Screen from "./Screen";
 import {
   MaterialCommunityIcons,
   Ionicons,
   Entypo,
   Feather,
 } from "@expo/vector-icons";
-import Overlay from "react-native-modal-overlay";
+import CheckBox from "react-native-check-box";
 
 const appIcon = require("../assets/logo.png");
+const iconSize = 24;
 
 function WelcomeDialogPrompt({
   title,
-  message,
-  content,
   positive,
   onPositiveButtonPress,
   isVisible = false,
@@ -26,157 +26,131 @@ function WelcomeDialogPrompt({
   const [dontShowAgainChecked, setDontShowAgainChecked] = useState(false);
 
   const windowWidth = Dimensions.get("window").width - 160;
-  const windowHeight = Dimensions.get("window").height;
-  const iconSize = 24;
 
   const handleDontShowAgain = () => {
     setDontShowAgainChecked(!dontShowAgainChecked);
   };
 
   return (
-    <Overlay
-      visible={isVisible}
-      animationType="zoomIn"
-      containerStyle={{
-        backgroundColor: defaultStyles.colors.semiTransparentGray,
-        padding: 12,
-      }}
-      childrenWrapperStyle={{
-        backgroundColor: defaultStyles.colors.white,
-        padding: 0,
-      }}
-      animationDuration={500}
-    >
-      {/* <ScrollView> */}
-      <Image style={styles.smallLogo} source={appIcon} />
-      <Text style={styles.title}>{title}</Text>
+    <Overlay isVisible={isVisible}>
+      <Screen>
+        <View style={styles.container}>
+          <Image style={styles.smallLogo} source={appIcon} />
+          <Text style={styles.title}>{title}</Text>
 
-      {/* {message && <Text style={styles.message}>{message}</Text>} */}
-      {/* {content && <Text style={styles.content}>{content}</Text>} */}
-      <Text style={styles.message}>
-        Regatta Comp Time allows you to quickly compare boat results after a
-        race. Use Time Delta section for time differences based on boat's
-        ratings. Use the Race section to track boat's compensated/corrected time
-        in "Real Time".
-      </Text>
+          <Dialog.ScrollArea contentContainerStyle={{ marginLeft: 0 }}>
+            <ScrollView contentContainerStyle={styles.scrollviewContainer}>
+              <Text style={styles.message}>
+                Regatta Comp Time allows you to quickly compare boat results
+                after a race. Use Time Delta section for time differences based
+                on boat's ratings. Use the Race section to track boat's
+                compensated/corrected time in "Real Time".
+              </Text>
 
-      <View style={styles.sectionContent}>
-        <SectionHelp
-          icon={
-            <MaterialCommunityIcons
-              name="sail-boat"
-              size={iconSize}
-              color={defaultStyles.colors.primary}
-            />
-          }
-          iconLabel="Fleet"
-          message={`The Fleet section is where you manage the boats that will be used in the Time Delta and Race sections.`}
-          width={windowWidth}
-        />
+              <SectionHelp
+                icon={
+                  <MaterialCommunityIcons
+                    name="sail-boat"
+                    size={iconSize}
+                    color={defaultStyles.colors.primary}
+                  />
+                }
+                iconLabel="Fleet"
+                message={`The Fleet section is where you manage the boats that will be used in the Time Delta and Race sections.`}
+                width={windowWidth}
+              />
 
-        <SectionHelp
-          icon={
-            <Entypo
-              name="stopwatch"
-              size={iconSize}
-              color={defaultStyles.colors.primary}
-            />
-          }
-          iconLabel="Time Delta"
-          message={`The Time Delta section is were you can compare the compensated time difference between boats in the Fleet based on the race duration of your boat.`}
-          width={windowWidth}
-        />
+              <SectionHelp
+                icon={
+                  <Entypo
+                    name="stopwatch"
+                    size={iconSize}
+                    color={defaultStyles.colors.primary}
+                  />
+                }
+                iconLabel="Time Delta"
+                message={`The Time Delta section is were you can compare the compensated time difference between boats in the Fleet based on the race duration of your boat.`}
+                width={windowWidth}
+              />
 
-        <SectionHelp
-          icon={
-            <Feather
-              name="flag"
-              size={iconSize}
-              color={defaultStyles.colors.primary}
-            />
-          }
-          iconLabel="Race"
-          message={`The Race section allows you to track realtime results (Corrected Time and Elapsed race time) of boats in the Fleet.`}
-          width={windowWidth}
-        />
+              <SectionHelp
+                icon={
+                  <Feather
+                    name="flag"
+                    size={iconSize}
+                    color={defaultStyles.colors.primary}
+                  />
+                }
+                iconLabel="Race"
+                message={`The Race section allows you to track realtime results (Corrected Time and Elapsed race time) of boats in the Fleet.`}
+                width={windowWidth}
+              />
 
-        <SectionHelp
-          icon={
-            <Feather
-              name={"settings"}
-              size={iconSize}
-              color={defaultStyles.colors.primary}
-            />
-          }
-          iconLabel="Settings"
-          message={`The Settings section allows you to apply global parameters that will affect all sections.`}
-          width={windowWidth}
-        />
-        <SectionHelp
-          icon={
-            <Ionicons
-              name="ios-help-circle-outline"
-              size={iconSize}
-              color={defaultStyles.colors.primary}
-            />
-          }
-          width={windowWidth}
-          message={`Displays the Help of a Section. `}
-        />
-      </View>
+              <SectionHelp
+                icon={
+                  <Feather
+                    name={"settings"}
+                    size={iconSize}
+                    color={defaultStyles.colors.primary}
+                  />
+                }
+                iconLabel="Settings"
+                message={`The Settings section allows you to apply global parameters that will affect all sections.`}
+                width={windowWidth}
+              />
+              <SectionHelp
+                icon={
+                  <Ionicons
+                    name="ios-help-circle-outline"
+                    size={iconSize}
+                    color={defaultStyles.colors.primary}
+                  />
+                }
+                message={`Displays the Help of a Section. `}
+                width={windowWidth}
+              />
+            </ScrollView>
+          </Dialog.ScrollArea>
 
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "center",
-          alignItems: "center",
-          padding: 4,
-          marginTop: 16,
-        }}
-      >
-        <CheckBox
-          style={styles.checkbox}
-          isChecked={dontShowAgainChecked}
-          onClick={handleDontShowAgain}
-          checkBoxColor={defaultStyles.colors.primary}
-        />
-        <Text>Don't show again</Text>
-      </View>
-
-      <View style={styles.buttonContainer}>
-        {positive && (
-          <Button
-            buttonStyle={styles.button}
-            title={positive}
-            onPress={onPositiveButtonPress}
-          />
-        )}
-      </View>
-      {/* </ScrollView> */}
+          <View style={styles.buttonContainer}>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+                padding: 4,
+                marginTop: 16,
+              }}
+            >
+              <CheckBox
+                style={styles.checkbox}
+                isChecked={dontShowAgainChecked}
+                onClick={handleDontShowAgain}
+                checkBoxColor={defaultStyles.colors.primary}
+              />
+              <Text>Don't show again</Text>
+            </View>
+            {positive && (
+              <Button
+                buttonStyle={styles.button}
+                title={positive}
+                onPress={onPositiveButtonPress}
+              />
+            )}
+          </View>
+        </View>
+      </Screen>
     </Overlay>
   );
 }
 
 const styles = StyleSheet.create({
-  container(width, height) {
-    return {
-      marginTop: 64,
-      width: width,
-      height: height,
-      backgroundColor: defaultStyles.colors.white,
-    };
+  container: {
+    flex: 1,
+    marginBottom: 8,
+    backgroundColor: defaultStyles.colors.transparent,
   },
-  checkbox: {
-    right: 0,
-    marginLeft: 24,
-    marginRight: 4,
-    justifyContent: "center",
-    alignSelf: "center",
-  },
-  checkboxText: {
-    color: "red",
-  },
-  scrollviewContainer: {},
+  scrollviewContainer: { paddingBottom: 16 },
   content: {
     alignSelf: "center",
     marginTop: 24,
@@ -184,35 +158,29 @@ const styles = StyleSheet.create({
     marginRight: 16,
     backgroundColor: defaultStyles.colors.transparent,
   },
-  button: { minWidth: 100, margin: 4, marginTop: 24, marginBottom: 16 },
-  buttonContainer: { flexDirection: "row", justifyContent: "center" },
+  button: { minWidth: 100, margin: 4, marginTop: 16 },
+  buttonContainer: { justifyContent: "center", alignItems: "center" },
   title: {
-    // marginTop: Platform.OS === "ios" ? 24 : -18,
-    marginTop: 8,
     justifyContent: "center",
     alignSelf: "center",
     color: defaultStyles.colors.primary,
     fontWeight: "600",
-    fontSize: 24,
+    fontSize: 18,
   },
-  message: {
-    justifyContent: "flex-start",
-    alignSelf: "flex-start",
-    marginLeft: 14,
-    marginTop: 8,
-    fontSize: 14,
-  },
+  message: { marginTop: 16 },
   smallLogo: {
-    marginTop: 16,
-    height: 80,
+    marginTop: 8,
+    marginBottom: 8,
+    width: 70,
+    height: 70,
     resizeMode: "contain",
     alignSelf: "center",
   },
-  sectionContent: {
-    width: 180,
-    justifyContent: "flex-start",
-    alignSelf: "flex-start",
-    marginTop: 8,
+  checkbox: {
+    right: 0,
+    marginRight: 4,
+    justifyContent: "center",
+    alignSelf: "center",
   },
 });
 
@@ -238,9 +206,8 @@ const sectionStyles = StyleSheet.create({
       flexDirection: "row",
       justifyContent: "flex-start",
       alignItems: "flex-start",
-      margin: 4,
-      marginLeft: 8,
       width: width,
+      marginTop: 8,
     };
   },
   iconContainer: {
