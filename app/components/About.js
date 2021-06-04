9ae7ec1070550d8003a615c3ade5eb689c058c47
Luc Gregoire <lucgmix@gmail.com>
2021-06-04 10:20:11 -0400

    Beta 3

--- app/components/About.js
+++ app/components/About.js
@@ -14,7 +14,7 @@ function About() {
         {/* <Text style={styles.text}>
           App Version: {Constants.manifest.version}
         </Text> */}
-        <Text style={styles.text}>App Version: Beta 2.0</Text>
+        <Text style={styles.text}>App Version: Beta 3</Text>
         <Text style={styles.text}>
           Sdk Version: {Constants.manifest.sdkVersion}
         </Text>
