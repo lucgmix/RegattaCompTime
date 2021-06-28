ec703c6312565cc7051930eabb05d5c66019a190
Luc Gregoire <lucgmix@gmail.com>
2021-06-28 09:12:08 -0400

    Added original boat list from PHRF with updated list cleaning.

--- app/utils/boatListCleaner.js
+++ app/utils/boatListCleaner.js
@@ -1,7 +1,10 @@
+import { words } from "lodash";
+
 export const capitalizeTheFirstLetterOfEachWord = (words) => {
   let separateWord = words.split(" ");
   for (var i = 0; i < separateWord.length; i++) {
-    if (!isExcludedWords(separateWord[i])) {
+    separateWord[i] = excludeChars(separateWord[i]);
+    if (!keepCapitalWords(separateWord[i])) {
       separateWord[i] = separateWord[i].toLowerCase();
     }
     separateWord[i] =
@@ -10,13 +13,27 @@ export const capitalizeTheFirstLetterOfEachWord = (words) => {
   return separateWord.join(" ");
 };
 
-const isExcludedWords = (word) => {
-  const excludedWords = [
+const excludeChars = (word) => {
+  let cleanedWord = "";
+  const excludedWords = ["*"];
+  for (let i = 0; i < excludedWords.length; i++) {
+    cleanedWord = word.replaceAll(excludedWords[i], "");
+  }
+  return cleanedWord;
+};
+
+const keepCapitalWords = (word) => {
+  const keepCapWords = [
+    "BS",
     "C&C",
+    "CB",
     "CC",
+    "CF",
     "CS",
     "CAL",
+    "DB",
     "DB1",
+    "DK",
     "DS",
     "G&S",
     "GT",
@@ -25,8 +42,38 @@ const isExcludedWords = (word) => {
     "IOR",
     "KDY",
     "S&S",
+    "SD",
     "US",
     "VX",
+    "OB",
+    "-SB",
+    "IB",
+    "TM",
+    "KCB",
+    "SK",
+    "DP",
+    "SD/WK",
+    "WK/DK",
+    "FR",
+    "WK",
+    "(TK)",
+    "WKI",
+    "SM",
+    "CF",
+    "FK",
+    "SQ",
+    "J/V",
+    "LB",
+    "FXK",
+    "MH",
+    "(XL)",
+    "SJ",
+    "CIT",
+    "LI",
+    "RFB",
+    "(OB)",
+    "(C&C)",
+    "SHK",
   ];
-  return excludedWords.includes(word);
+  return keepCapWords.includes(word);
 };
